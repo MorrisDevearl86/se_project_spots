bec470d9b3dc58cc9b91a8dc4ed7d2cf5d9a56eb
MorrisDevearl86 <morrisdevearl@gmail.com>
2025-04-09 16:00:54 -0500

    update modal in html and index.js script

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+//Edit Profile Button ↓↓↓
+
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
@@ -5,5 +7,25 @@ const editProfileCloseButton = editProfileModal.querySelector(
 );
 
 editProfileButton.addEventListener("click", function () {
-  editProfileModal.classList.add("modal__close-button");
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+// New Post Button ↓↓↓
+
+const newPostProfileButton = document.querySelector(".profile__edit-button");
+const newPostProfileModal = document.querySelector("#new-post-modal");
+const newPostProfileCloseButton = newPostProfileModal.querySelector(
+  ".modal__close-button"
+);
+
+newPostProfileButton.addEventListener("click", function () {
+  newPostProfileModal.classList.add("modal_is-opened");
+});
+
+newPostProfileCloseButton.addEventListener("click", function () {
+  newPostProfileModal.classList.remove("modal_is-opened");
 });
