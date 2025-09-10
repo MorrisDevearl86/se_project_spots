0ec09dc0041cac92bef1919f00837329e0f9f6b7
Morris Devearl <Morrisdevearl@gmail.com>
2025-09-09 21:37:12 -0500

    update the validation.js.

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,4 +1,4 @@
-const showINputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.add("modal__input_type_error");
   errorElement.textContent = errorMessage;
@@ -28,18 +28,20 @@ const hasInvalidInput = (inputList) => {
 
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("modal__save-button_disabled");
+    buttonElement.classList.add("modal__submit-button_disabled");
     buttonElement.disabled = true;
   } else {
-    buttonElement.classList.remove("modal__save-button_disabled");
+    buttonElement.classList.remove("modal__submit-button_disabled");
     buttonElement.disabled = false;
   }
 } ;
 const setEventListeners = (formElement) => {
   const inputList = Array.from(
     formElement.querySelectorAll(".modal__input")
+
   ) ;
-  const buttonElement = formElement.querySelector(".modal__save-button");
+
+  const buttonElement = formElement.querySelector(".modal__submit-button");
   toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
@@ -62,142 +64,9 @@ const enableValidation = () => {
 enableValidation() ;
 
 export const resetValidation = (formElement, inputList) => {
-  const buttonElement = formElement.querySelector(".modal__save-button");
+  const buttonElement = formElement.querySelector(".modal__submit-button");
   inputList.forEach((inputElement) => {
     hideInputError(formElement, inputElement);
   }) ;
   toggleButtonState(inputList, buttonElement);
 } ;
-
-// // ----------------- Other code in the file -----------------
-// import { openModal, closeModal } from "./modal.js";
-
-// const nameInput = document.querySelector("#profile-name-input");
-
-// // Example initial cards
-// export const initialCards = [
-//   {
-//     name: "Yosemite Valley",
-//     link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-//   },
-//   {
-//     name: "Lake Louise",
-//     link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-//   },
-//   {
-//     name: "Bald Mountains",
-//     link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-//   },
-//   {
-//     name: "Latemar",
-//     link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-//   },
-//   {
-//     name: "Vanoise National Park",
-//     link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-//   },
-//   {
-//     name: "Lago di Braies",
-//     link: "https://code.s3.yandex.net/web-code/lago.jpg",
-//   },
-
-//   {
-//     name: "Forest creek",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-mike-kenneally-from-pexels.jpg",
-//   },
-//   {
-//     name: "Dandelion",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-pixabay-from-pexels.jpg",
-//   },
-//   {
-//     name: "Snowy mountains",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-luca-bergamini-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "City on the water",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-mike-kenneally-from-pexels.jpg",
-//   },
-//   {
-//     name: "Park bench",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-pixabay-from-pexels.jpg",
-//   },
-//   {
-//     name: "Snowy mountains",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-luca-bergamini-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "City on the water",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "Park bench",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "Snowy mountains",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "Sunset over the mountains",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-//   {
-//     name: "Forest road",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-//   },
-//   {
-//     name: "City skyline",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "Calm lake",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-//   {
-//     name: "Desert dunes",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-//   },
-//   {
-//     name: "Snowy peaks",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "Ocean view",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-//   {
-//     name: "Countryside road",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-//   },
-//   {
-//     name: "Rocky cliffs",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-
-//   {
-//     name: "Sunny beach",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-//   {
-//     name: "Misty forest",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-
-//   },  {
-//     name: "City park",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-
-
-
-
-
-
-
-
